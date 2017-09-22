ec37267cef2677685f0892785565df67ad6e06bd
xuepeng <helloxuepeng@gmail.com>
2017-09-22 11:24:39 +0800

    简单搜索

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 import markdown
 from markdown.extensions.toc import TocExtension
 from django.utils.text import slugify
+from django.db.models import Q
 
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
@@ -175,19 +176,17 @@ class TagView(ListView):
         return super(TagView, self).get_queryset().filter(tags=tag)
 
 
-"""
-请使用下方包含评论列表和评论表单的详情页视图
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
-    return render(request, 'blog/detail.html', context={'post': post})
-"""
+def search(request):
+    q = request.GET.get('q')
+    error_msg = ''
 
+    if not q:
+        error_msg = "请输入关键词"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+
+    post_list = Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request, 'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
